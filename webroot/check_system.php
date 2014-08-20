2b0b3859a7f12c24e2af3adb0b1d10cc39352a0b
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-08-21 00:31:05 +0200

    Extended info printed #35

--- webroot/check_system.php
+++ webroot/check_system.php
@@ -1,4 +1,12 @@
 <?php
 
+echo 'Current PHP version: ' . phpversion() . '<br><br>';
+
+echo 'Running on: ' . $_SERVER['SERVER_SOFTWARE'] . '<br><br>';
+
 $no = extension_loaded('gd') ? null : 'NOT';
-echo "Extension gd is $no loaded.";
+echo "Extension gd is $no loaded.<br>";
+
+if (!$no) {
+    var_dump(gd_info());
+}
